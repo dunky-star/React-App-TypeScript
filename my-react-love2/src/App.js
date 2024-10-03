cfd327338b2e0d896aefa45564e150dd006e217c
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-02 21:43:35 -0500

    Converting HTML to a React

--- my-react-love2/src/App.js
+++ my-react-love2/src/App.js
@@ -1,3 +1,19 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import { NavBar } from "./components/NavBar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
 };
